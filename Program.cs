a813f7fefc8fcbddc2577cb80dae3d00ac350a21
Genildon Barreto <genildonbarreto@yahoo.com.br>
2025-06-09 08:04:11 -0300

    Feitas correções na jogabilidade, alterando a entrada do usuário de digitar o nome da opção para um número associado a opção desejada; O usuário não necessita mais jogar até que ele ou o oponente tenha 3 pontos; Adicionado operador de coalescência nula "??" nas entradas de dados do usuário para que o valor da string caso não seja digitado não seja nulo; Alteradas condicionais switch para if a fim de simplificar o código; Reduzida a quantidade de mensagens do console sobre as pontuações.

--- Program.cs
+++ Program.cs
@@ -11,103 +11,82 @@ namespace pedraPapelTesoura
 
             int pontuacaoJogador = 0;
             int pontuacaoOponente = 0;
+            int totalPartidas = 0;
 
-            Console.WriteLine("Bem-vindo ao pedra, papel, tesoura!\n");
+            Console.WriteLine("--- Bem-vindo ao pedra, papel e tesoura! ---");
+            Console.WriteLine("Por favor insira a quantidade de partidas que deseja jogar:");
 
-            while (pontuacaoJogador != 3 && pontuacaoOponente != 3)
+            while (true)
             {
-                Console.WriteLine($"Pontuação do jogador - {pontuacaoJogador}. Pontuação do oponente - {pontuacaoOponente}\n");
-                Console.WriteLine($"Por favor insira 'pedra' para pedra, 'papel' para papel, 'tesoura' para tesoura e 'sair' para sair");
-                string escolhaJogador = Console.ReadLine();
-
-                if (escolhaJogador == "sair")
+                string quantidadeTotalPartidas = Console.ReadLine() ?? "";
+                if (!int.TryParse(quantidadeTotalPartidas, out totalPartidas))
                 {
-                    Console.WriteLine($"Pontuação: Jogador - {pontuacaoJogador}. Oponente - {pontuacaoOponente}\n");
-                    break;
-                }
-
-                int escolhaOponente = random.Next(0, 3);
-
-                if (escolhaOponente == 0)
-                {
-                    Console.WriteLine("\nOponente escolheu pedra.");
-
-                    switch (escolhaJogador)
-                    {
-                        case "pedra":
-                            Console.WriteLine("Empate!");
-                            break;
-                        case "papel":
-                            Console.WriteLine("Jogador ganhou essa rodada.");
-                            pontuacaoJogador++;
-                            break;
-                        default:
-                            Console.WriteLine("Oponente ganhou essa rodada.");
-                            pontuacaoOponente++;
-                            break;
-                    }
-                }
-                else if (escolhaOponente == 1)
-                {
-                    Console.WriteLine("\nOponente escolheu papel.");
-
-                    switch (escolhaJogador)
-                    {
-                        case "pedra":
-                            Console.WriteLine("Oponente ganhou essa rodada.");
-                            pontuacaoOponente++;
-                            break;
-                        case "papel":
-                            Console.WriteLine("Empate!");
-                            break;
-                        default:
-                            Console.WriteLine("Jogador ganhou essa rodada.");
-                            pontuacaoJogador++;
-                            break;
-                    }
+                    Console.WriteLine("Escolha inválida, favor insira numericamente a quantidade de partidas que deseja participar");
                 }
                 else
                 {
-                    Console.WriteLine("\nOponente escolheu tesoura.");
-
-                    switch (escolhaJogador)
-                    {
-                        case "pedra":
-                            Console.WriteLine("Jogador ganhou essa rodada.");
-                            pontuacaoJogador++;
-                            break;
-                        case "papel":
-                            Console.WriteLine("Oponente ganhou essa rodada.");
-                            pontuacaoOponente++;
-                            break;
-                        default:
-                            Console.WriteLine("Empate!");
-                            break;
-                    }
+                    Console.WriteLine($"\nVocê escolheu jogar {totalPartidas} partidas.");
+                    break;
                 }
             }
 
-            if (pontuacaoJogador == 3)
+            int rodadasJogadas = 0;
+            while (rodadasJogadas < totalPartidas)
             {
-                Console.WriteLine($@"Você ganhou! 
-                Pontuação:
-                    Jogador - {pontuacaoJogador}.
-                    Oponente - {pontuacaoOponente}.");
+                Console.WriteLine($"Pontuação do jogador - {pontuacaoJogador}.\nPontuação do oponente - {pontuacaoOponente}");
+                Console.WriteLine($"\nPor favor insira 1 para Pedra, 2 para Papel, 3 para Tesoura e '0' para Sair");
+                string escolhaDoJogador = Console.ReadLine() ?? "";
+
+                if (!int.TryParse(escolhaDoJogador, out int escolhaJogador))
+                {
+                    Console.WriteLine("Escolha inválida, favor insira 1 para Pedra, 2 para Papel ou 3 para Tesoura");
+                    continue;
+                }
+
+                string nomeEscolhaJogador = "";
+                if (escolhaJogador == 1) nomeEscolhaJogador = "Pedra";
+                else if (escolhaJogador == 2) nomeEscolhaJogador = "Papel";
+                else if (escolhaJogador == 3) nomeEscolhaJogador = "Tesoura";
+                Console.WriteLine($"Você escolheu {nomeEscolhaJogador}!");
+
+                if (escolhaJogador == 0)
+                {
+                    Console.WriteLine($"\nVocê decidiu sair do jogo.");
+                    break;
+                }
+
+                int escolhaOponente = random.Next(1, 4);
+
+                if (escolhaOponente == escolhaJogador)
+                {
+                    Console.WriteLine("\nEmpate!");
+                }
+                else if ((escolhaJogador == 1 && escolhaOponente == 3) || (escolhaJogador == 2 && escolhaOponente == 1) || (escolhaJogador == 3 && escolhaOponente == 2))
+                {
+                    Console.WriteLine("\nJogador ganhou essa rodada.");
+                    pontuacaoJogador++;
+                }
+                else
+                {
+                    Console.WriteLine("Oponente ganhou essa rodada.");
+                    pontuacaoOponente++;
+                }
+                rodadasJogadas++;
             }
-            else if (pontuacaoOponente == 3)
-            {
-                Console.WriteLine($@"Oponente ganhou!
-                Pontuação:
-                    Jogador - {pontuacaoJogador}.
-                    Oponente - {pontuacaoOponente}.");
-            }
-            else
-            {
-                Console.WriteLine($@"Você desistiu.
-                Pontuação:
-                    Jogador - {pontuacaoJogador}.
-                    Oponente - {pontuacaoOponente}.");
-            }
+
+
+                Console.WriteLine("\n --- Fim de Jogo ---");
+                if (pontuacaoJogador > pontuacaoOponente)
+                {
+                    Console.WriteLine($"Você ganhou!!!");
+                }
+                else if (pontuacaoOponente > pontuacaoJogador)
+                {
+                    Console.WriteLine($"Oponente ganhou!!!");
+                }
+                Console.WriteLine($@"Pontuação:
+                Jogador - {pontuacaoJogador}.
+                Oponente - {pontuacaoOponente}.");
         }
     }
 }
